77a6a7abb9728510dc0ac39dad89c921a643c303
Vojtech Vanek <vanevo00@gmail.com>
2020-09-20 22:28:21 +0200

    feat: get user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,9 @@
-const express = require ('express')
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const Profile = require('../models/Profile')
+const User = require('../models/User')
 
 const router = express.Router()
 
@@ -8,5 +13,30 @@ const router = express.Router()
 router.get('/test', (req, res) => res.json({
   message: 'test successful'
 }))
+
+// @route GET api/profile/
+// @desc fetch current user's profile
+// @access private
+router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    const {
+      user: {
+        id
+      }
+    } = req
+
+    const errors = {}
+
+    const foundProfile = await Profile.findOne({ user: id })
+    if (!foundProfile) {
+      errors.noprofile = 'There is no profile for this user'
+      return res.status(404).json(errors)
+    }
+
+    res.json(foundProfile)
+  } catch(err) {
+    console.error(err)
+  }
+})
 
 module.exports = router
