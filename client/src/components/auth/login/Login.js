57c0978b317c82f1ac7d19b217e69129283967b3
Vojtech Vanek <vanevo00@gmail.com>
2020-09-27 19:06:17 +0200

    feat: login form

--- client/src/components/auth/login/Login.js
+++ client/src/components/auth/login/Login.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [errors, setErrors] = useState({})
+
+  const onChange = (e) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputValues)
+  }
+
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">Sign in to your DevConnector account</p>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address" name="email"
+                  value={inputValues.email}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={inputValues.password}
+                  onChange={onChange}
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4"/>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
