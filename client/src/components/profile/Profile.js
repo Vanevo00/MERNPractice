415c0e12b816382869a8234dec14a25898c5570e
Vojtech Vanek <vanevo00@gmail.com>
2020-10-11 16:01:22 +0200

    feat: profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,8 +5,38 @@ import ProfileHeader from './ProfileHeader'
 import ProfileAbout from './ProfileAbout'
 import ProfileCreds from './ProfileCreds'
 import ProfileGithub from './ProfileGithub'
+import Spinner from '../common/Spinner'
+import { Link } from 'react-router-dom'
 
 const Profile = ({ profile, getProfileByHandle, match }) => {
+  const {
+    profile: foundProfile,
+    loading
+  } = profile
+
+  let profileContent
+
+  if (foundProfile === null || loading) {
+    profileContent = <Spinner/>
+  } else {
+    profileContent = (
+      <div>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Link to='/profiles' className='btn btn-light mb-3 float-left'>
+              Back To Profiles
+            </Link>
+          </div>
+          <div className='col-md-6'/>
+        </div>
+        <ProfileHeader profile={foundProfile}/>
+        <ProfileAbout/>
+        <ProfileCreds/>
+        <ProfileGithub/>
+      </div>
+    )
+  }
+
   useEffect(() => {
     if (match.params.handle) {
       getProfileByHandle(match.params.handle)
@@ -14,11 +44,14 @@ const Profile = ({ profile, getProfileByHandle, match }) => {
   }, [])
 
   return (
-    <div>
-      <ProfileHeader/>
-      <ProfileAbout/>
-      <ProfileCreds/>
-      <ProfileGithub/>
+    <div className='profile'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-12'>
+            {profileContent}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,9 +1,69 @@
 import React from 'react'
+import isEmpty from '../../validation/isEmpty'
 
-const ProfileHeader = () => {
+const ProfileHeader = ({ profile }) => {
+  const {
+    user: {
+      avatar,
+      name
+    },
+    status,
+    company,
+    location,
+    website,
+    social: {
+      twitter,
+      facebook,
+      instagram,
+      linkedin,
+      youtube
+    } = {}
+  } = profile
+
   return (
-    <div>
-      <h1>PROFILE HEADER</h1>
+    <div className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-info text-white mb-3">
+          <div className="row">
+            <div className="col-4 col-md-3 m-auto">
+              <img className="rounded-circle" src={avatar} alt={name}/>
+            </div>
+          </div>
+          <div className="text-center">
+            <h1 className="display-4 text-center">{name}</h1>
+            <p className="lead text-center">{status} {isEmpty(company) ? null : (<span>at {company}</span>)}</p>
+            {isEmpty(location) ? null : (<p>{location}</p>)}
+            <p>
+              {isEmpty(website) ? null : (
+                <a className="text-white p-2" href={website} target='_blank'>
+                  <i className="fas fa-globe fa-2x"></i>
+                </a>
+              )}
+              {isEmpty(twitter) ? null : (
+                <a className="text-white p-2" href={twitter} target='_blank'>
+                  <i className="fab fa-twitter fa-2x"></i>
+                </a>
+              )}
+              {isEmpty(facebook) ? null : (
+                <a className="text-white p-2" href={facebook} target='_blank'>
+                  <i className="fab fa-facebook fa-2x"></i>
+                </a>
+              )}
+              {isEmpty(linkedin) ? null : (
+                <a className="text-white p-2" href={linkedin} target='_blank'>
+                  <i className="fab fa-linkedin fa-2x"></i>
+                </a>
+              )}
+              {isEmpty(youtube) ? null : (
+                <a className="text-white p-2" href={youtube} target='_blank'>
+                  <i className="fab fa-youtube fa-2x"></i>
+                </a>
+              )}
+
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
